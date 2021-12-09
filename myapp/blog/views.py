1cc775ca8d3c7ec9fceef7f36b4843313b131fe4
Akhil <akhil.h@bridge-global.com>
2021-12-09 17:37:20 +0530

    listing data

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,7 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse, request
+from .models import PostModel
 
 # Create your views here.
 def page1(request):
-    return HttpResponse("Here's the text of the Web page.")
+    qs = PostModel.objects.all()
+    template = 'list-blog.html'
+    context_dict = {
+        'object_list': qs
+    }
+    return render(request, template, context_dict)
