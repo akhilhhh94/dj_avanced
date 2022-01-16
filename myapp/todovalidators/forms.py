03ddfd47eb8541543e23730a66b4c14c138eedb3
Akhil <akhil.h@bridge-global.com>
2022-01-16 18:15:45 +0530

    validator clean methods

--- myapp/todovalidators/forms.py
+++ myapp/todovalidators/forms.py
@@ -1,3 +1,4 @@
+from xml.dom import ValidationErr
 from django import forms
 
 from .models import todo
@@ -8,3 +9,16 @@ class TodoForms(forms.ModelForm):
         fields = [
             'title'
         ]
+    def clean_title(self):
+        title = self.cleaned_data.get("title",  None)
+        if title == "akhil":       
+            raise ValidationErr("Akhil not allowd")
+        
+        return title
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        if title == "amela":       
+            raise ValidationErr("amela not allowd")
+        
--- myapp/todovalidators/validator.py
+++ myapp/todovalidators/validator.py
@@ -3,7 +3,6 @@ from django.core.exceptions import ValidationError
 
 def validate_even(value):
     value = set(f"{value}".lower().split())
-    print(value)
     if len(value & set(["bad", "dad"])) > 0:
         raise ValidationError(
             ('%(value)s is not an even number'),
