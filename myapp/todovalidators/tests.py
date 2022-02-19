6699f66e858be319a35c4ef3ed46aafaa07a8b03
Akhil <akhil@sample>
2022-02-19 22:57:51 +0530

    Sample test added

--- myapp/todovalidators/tests.py
+++ myapp/todovalidators/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import todo
 
-# Create your tests here.
+class TodoTestCase(TestCase):
+    def create_item(self):
+        data = {
+            "title": "akhil"
+        }
+        todo.objects.create(**data)
+
+    def setUp(self) -> None:
+        self.create_item()
+
+    def test_draft_items(self):
+        qs = todo.objects.filter(publish_status=todo.PublishCoice.PUBLISHED)
+        self.assertTrue(qs.exists())
