90c519847e0e8d4f94045c9a746bab99fddedd6a
Akhil <akhil@sample>
2022-02-21 22:36:15 +0530

    Sample test added -2

--- myapp/todovalidators/tests.py
+++ myapp/todovalidators/tests.py
@@ -1,16 +1,38 @@
 from django.test import TestCase
 from .models import todo
 
+
 class TodoTestCase(TestCase):
     def create_item(self):
         data = {
             "title": "akhil"
         }
         todo.objects.create(**data)
+        todo.objects.create(**data)
+        todo.objects.create(**data)
+        self.publishedCount = 3
+
+    def create_draft_item(self):
+        data = {
+            "title": "akhil",
+            "publish_status": todo.PublishCoice.DRAFT
+        }
+        todo.objects.create(**data)
+        todo.objects.create(**data)
+        self.draftCount = 2
 
     def setUp(self) -> None:
         self.create_item()
+        self.create_draft_item()
 
-    def test_draft_items(self):
+    def test_published_sample(self):
         qs = todo.objects.filter(publish_status=todo.PublishCoice.PUBLISHED)
         self.assertTrue(qs.exists())
+
+    def test_published_items(self):
+        qs = todo.objects.filter(publish_status=todo.PublishCoice.PUBLISHED)
+        self.assertEqual(qs.count(), self.publishedCount)
+
+    def test_draft_items(self):
+        qs = todo.objects.filter(publish_status=todo.PublishCoice.DRAFT)
+        self.assertEqual(qs.count(), self.draftCount)
