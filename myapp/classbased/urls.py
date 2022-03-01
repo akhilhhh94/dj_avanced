45c30b3e168ecf0789464668cf90c6c5992cc8a3
Akhil <akhil@sample>
2022-03-01 23:57:21 +0530

    RedirectView view(URl redirection)

--- myapp/classbased/urls.py
+++ myapp/classbased/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .views import getDateTime
 
 urlpatterns = [
     path('/about', TemplateView.as_view(template_name="about_us.html"), name='get_date'),
+    path('/about-us', RedirectView.as_view(url="/sample/about")),
     path('/', getDateTime, name='get_date')
 ]
